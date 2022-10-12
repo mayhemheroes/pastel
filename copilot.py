5775ff2867895a0d16ccf202daa4e2f6e8c1dec0
Dylan <57853417+4812571@users.noreply.github.com>
2022-10-12 15:28:19 -0400

    mayhem integration

---
+++ copilot.py
@@ -0,0 +1,13 @@
+# What is Docker?
+# Docker is a set of platform as a service products that use OS-level virtualization to deliver software in packages called containers.
+
+# What are containers?
+# Containers are isolated environments that run independently from one another. They are similar to virtual machines, but they are more lightweight and more portable.
+
+# Why use containers?
+# Containers are useful for running applications that require a lot of dependencies, such as databases, web servers, and other applications that require a lot of resources. Containers are also useful for running applications that require different versions of the same dependency, such as different versions of Python.
+# Containers are also useful for running applications that require different versions of the same dependency, such as different versions of Python.
+
+# What is Docker Compose?
+# Docker Compose is a tool for defining and running multi-container Docker applications. With Compose, you use a YAML file to configure your application’s services. Then, with a single command, you create and start all the services from your configuration.
+
